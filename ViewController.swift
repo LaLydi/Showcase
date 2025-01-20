13ecc8feaec4b7ba310d5daa0273dad4a7ba2d50
Lydia <lydiamariong@gmail.com>
2025-01-20 14:41:11 -0700

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Showcase
+//
+//  Created by Lydia on 2025-01-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
