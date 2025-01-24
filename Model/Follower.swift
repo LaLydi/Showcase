baafcc68102ecb686181cb661157f6916c0cfb98
Lydia <lydiamariong@gmail.com>
2025-01-24 12:48:46 -0700

    created follower and user model

---
+++ Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  Showcase
+//
+//  Created by Lydia on 2025-01-24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
