3df3444749c00389d1f203cf1fdfbff8a08fe480
Lydia <lydiamariong@gmail.com>
2025-01-28 16:50:56 -0700

    added download and cache for avatar images

--- Views/ImageView/GFAvatarImageView.swift
+++ Views/ImageView/GFAvatarImageView.swift
@@ -10,6 +10,7 @@ import UIKit
 class GFAvatarImageView: UIImageView {
     
     let placeholderImage = UIImage(named: "avatar-placeholder")!
+    let cache = NetworkManager.shared.cache
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,4 +28,24 @@ class GFAvatarImageView: UIImageView {
         translatesAutoresizingMaskIntoConstraints = false
     }
     
+    func downloadImage(from url: String) {
+        let cacheKey = NSString(string: url)
+        if let cachedImage = cache.object(forKey: cacheKey) {
+            self.image = cachedImage
+            return
+        }
+        
+        guard let url = URL(string: url) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil else { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            guard let image = UIImage(data: data) else { return }
+            self.cache.setObject(image, forKey: cacheKey)
+            
+            DispatchQueue.main.async { self.image = image }
+        }
+        task.resume()
+    }
 }
