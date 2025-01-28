4a1c6c4e66be5c23d1e51f1ba02d4867ee481973
Lydia <lydiamariong@gmail.com>
2025-01-28 11:30:45 -0700

    refactored network call to result type

--- Controllers/FollowerListViewController.swift
+++ Controllers/FollowerListViewController.swift
@@ -10,20 +10,19 @@ import UIKit
 class FollowerListViewController: UIViewController {
     
     var username: String!
-    var followers: [Follower] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, error in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Something happened", message: error!.rawValue, buttonTitle: "Ok")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something happened", message: error.rawValue, buttonTitle: "Ok")
             }
-            print(followers.count)
-            print(followers)
         }
     }
     
