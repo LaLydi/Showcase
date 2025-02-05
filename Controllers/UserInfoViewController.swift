c784eb4db59d3f90b48240414cff6f456bec8bfd
Lydia <lydiamariong@gmail.com>
2025-02-05 12:16:51 -0700

    created userinfo network call

--- Controllers/UserInfoViewController.swift
+++ Controllers/UserInfoViewController.swift
@@ -16,6 +16,15 @@ class UserInfoViewController: UIViewController {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissViewController))
         navigationItem.rightBarButtonItem = doneButton
+        
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            switch result {
+                case .success(let user):
+                print(user)
+            case .failure(let error):
+                self?.presentGFAlertOnMainThread(title: "Something went wrong", message: error.localizedDescription, buttonTitle: "Ok")
+            }
+        }
     }
     
     @objc func dismissViewController() {
