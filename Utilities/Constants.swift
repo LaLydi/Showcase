0c2571f1d10c274a817bb37c1b275eda308fafc6
Lydia <lydiamariong@gmail.com>
2025-02-06 12:38:42 -0700

    created userInfoVC

---
+++ Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Showcase
+//
+//  Created by Lydia on 2025-02-06.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse.circle"
+}
